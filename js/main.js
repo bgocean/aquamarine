b26f1b3dcf5a98dfada8809f74d078e77f684e01
bgocean <bigocean700@gmail.com>
2024-04-17 15:31:49 +0300

    add medexam but specialists-slider not ended

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
-const swiper = new Swiper('.header__slider', {
+const headSwiper = new Swiper(".header__slider", {
+  spaceBetween: 10,
+  speed: 1000,
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true, // Делает поинты кликабельными
+  },
+});
+
+const spetsSwiper = new Swiper(".specialists__swiper", {
+  autoHeigt: true,
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  slidesPerView: "auto",
+  loop: true,
+  loopedSlides: 6,
+  speed: 1000,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    clickable: true,
   },
 });
