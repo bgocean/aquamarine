29bc6bf21dc2651456742d3cdc98dd1981f63229
bgocean <bigocean700@gmail.com>
2024-04-30 13:13:56 +0300

    add medexam-slider but not ended

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+const infoButton = document.querySelector('.info-button');
+const contextMenu = document.querySelector('.context-menu');
+
+infoButton.addEventListener('mouseover', () => {
+  contextMenu.style.display = 'block';
+});
+
+infoButton.addEventListener('mouseout', () => {
+  contextMenu.style.display = 'none';
+});
+
 const headSwiper = new Swiper(".header__slider", {
   spaceBetween: 10,
   speed: 1000,
@@ -9,10 +20,9 @@ const headSwiper = new Swiper(".header__slider", {
 
 const spetsSwiper = new Swiper(".specialists__swiper", {
   autoHeigt: true,
-  slidesPerView: 3,
   spaceBetween: 30,
   slidesPerGroup: 3,
-  slidesPerView: "auto",
+  slidesPerView: "auto",  
   loop: true,
   loopedSlides: 6,
   speed: 1000,
@@ -21,3 +31,46 @@ const spetsSwiper = new Swiper(".specialists__swiper", {
     clickable: true,
   },
 });
+
+const swiperMenu = new Swiper(".services__slider", {
+  slidesPerView: 1,
+  slidesPerColumn: 1,
+  speed: 800,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+let tabs = document.querySelector(".tabs");
+let menuSlider = document.querySelectorAll(".services__slider-block");
+
+tabs.addEventListener("click", function (e) {
+  if (e.target.classList.contains("tabs__btn")) {
+    this.querySelectorAll(".tabs__btn").forEach((item) =>
+      item.classList.remove("active")
+    );
+    e.target.classList.add("active");
+  }
+
+  menuSlider.forEach((item) => {
+    item.classList.remove("show");
+    let tabIndex = e.target.dataset.tab;
+    let thisSwiper = document.querySelector(tabIndex);
+    thisSwiper.classList.add("show");
+  });
+});
+
+window.addEventListener("load", overflowTabs);
+window.addEventListener("resize", overflowTabs);
+
+function overflowTabs() {
+  let outerTabs = document.querySelector(".tabs__container");
+  let innerTabs = document.querySelector(".tabs");
+  if (innerTabs.offsetWidth > outerTabs.offsetWidth) {
+    innerTabs.classList.add("overflow");
+  } else {
+    innerTabs.classList.remove("overflow");
+  }
+}
+
